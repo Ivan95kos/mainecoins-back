4ce5d3f223c605d1d9a3241e88441a8f925b9291
Ivan Kosovych <ivan95kos@gmail.com>
2019-04-05 22:49:27 +0300

    add simpleCORS

--- src/main/java/mainecoins/util/SimpleCORSFilter.java
+++ src/main/java/mainecoins/util/SimpleCORSFilter.java
@@ -13,7 +13,7 @@ public class SimpleCORSFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
         chain.doFilter(req, res);
     }
